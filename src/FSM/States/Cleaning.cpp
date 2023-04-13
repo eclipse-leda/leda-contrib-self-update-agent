1e9fdad38352efa198f4ffcb551129cdd8e60522
OleksandrChaika <oleksandr.chaika@bosch-softtec.com>
2023-04-13 11:02:16 +0200

    Add new states for Activating and Cleanup

---
+++ src/FSM/States/Cleaning.cpp
@@ -0,0 +1,30 @@
+//    Copyright 2022 Contributors to the Eclipse Foundation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+//    SPDX-License-Identifier: Apache-2.0
+
+#include "FSM/States/Cleaning.h"
+
+namespace sua {
+
+    Cleaning::Cleaning()
+        : Connected("Cleaning")
+    { }
+
+    void Cleaning::onEnter(Context& /*ctx*/)
+    {
+        // Do not send current version (as it was sent from 'Connected' state)
+    }
+
+} // namespace sua
---
+++ src/FSM/States/Cleaning.h
@@ -0,0 +1,33 @@
+//    Copyright 2022 Contributors to the Eclipse Foundation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+//    SPDX-License-Identifier: Apache-2.0
+
+#ifndef SDV_SUA_CLEANING_H
+#define SDV_SUA_CLEANING_H
+
+#include "FSM/States/Connected.h"
+
+namespace sua {
+
+    class Cleaning : public Connected {
+    public:
+        Cleaning();
+
+        void onEnter(Context& /*ctx*/) override;
+    };
+
+} // namespace sua
+
+#endif
