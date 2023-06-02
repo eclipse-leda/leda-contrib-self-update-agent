6c84a77642d39b38c96caa4267c6d330dfd49a8e
OleksandrChaika <oleksandr.chaika@bosch-softtec.com>
2023-06-02 11:11:43 +0200

    Enable unit-tests for arm64 architecture

--- scripts/build_glib_amd64.sh
+++ scripts/build_glib_amd64.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd 3rdparty/glib
-meson subprojects download
-meson setup -Dwrap_mode=forcefallback ../../build_amd64/glib
-meson compile -j `nproc` -C ../../build_amd64/glib
+meson subprojects download > /dev/null
+meson setup -Dwrap_mode=forcefallback ../../build_amd64/glib > /dev/null
+meson compile -j `nproc` -C ../../build_amd64/glib > /dev/null
