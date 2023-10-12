c413945d70001f98688aa1eb18d0f3457bf0e5ae
OleksandrChaika <99030120+OleksandrChaika@users.noreply.github.com>
2023-10-12 12:42:52 +0200

    Specify runtime for glib to /var/run (#48)

--- scripts/build_glib_amd64.sh
+++ scripts/build_glib_amd64.sh
@@ -2,5 +2,5 @@
 
 cd 3rdparty/glib
 meson subprojects download > /dev/null
-meson setup -Dwrap_mode=forcefallback ../../build_amd64/glib > /dev/null
+meson setup -Dwrap_mode=forcefallback ../../build_amd64/glib -Druntime_dir=/var/run > /dev/null
 meson compile -j `nproc` -C ../../build_amd64/glib > /dev/null
