0ef8253561361afd8644070c0c4b1cb48c036276
OleksandrChaika <oleksandr.chaika@bosch-softtec.com>
2023-10-12 11:53:02 +0200

    Specify runtime for glib to /var/run

--- scripts/build_glib_arm64.sh
+++ scripts/build_glib_arm64.sh
@@ -2,5 +2,5 @@
 
 cd 3rdparty/glib
 meson subprojects download > /dev/null
-meson setup -Dwrap_mode=forcefallback --cross-file=../meson-cross-file-aarch64.txt ../../build_arm64/glib > /dev/null
+meson setup -Dwrap_mode=forcefallback -Druntime_dir=/var/run --cross-file=../meson-cross-file-aarch64.txt ../../build_arm64/glib > /dev/null
 meson compile -j `nproc` -C ../../build_arm64/glib > /dev/null
